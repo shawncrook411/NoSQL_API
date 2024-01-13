affab8f3eef4e6d4fae18d713eda5c27eb7c2319
Shawn Crook <shawncrook411@gmail.com>
2024-01-12 21:39:09 -0700

    fixed update thoughts bug

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -6,23 +6,23 @@ const { Thought } = require('../models')
 module.exports = {
     async getThought(req, res){
         try {
-            const thought = await Thought.findOne({ _id: req.params.thoughtId }).populate('reactions')
+            const thought = await Thought.findOne({ _id: req.params.thoughtId }).select('-__v');
             if (!thought) return res.status(404).json({ message: 'No thought found' })
 
             res.json(thought)
         } catch (err) {
             console.error({ message: err })
-            res.status(500).json(err)
+            return res.status(500).json(err)
         }
     },
 
     async getThoughts(req, res){
         try {
-            const thoughts = await Thought.find().populate('reactions')
+            const thoughts = await Thought.find()
             res.json(thoughts)
         } catch (err) {
             console.error({ message: err })
-            res.status(500).json(err)
+            return res.status(500).json(err)
         }
     },
 
@@ -32,41 +32,41 @@ module.exports = {
             res.json(thought)
         } catch (err) {
             console.error({ message: err })
-            res.status(500).json(err)
+            return res.status(500).json(err)
         }
     },
 
     async deleteThought(req, res){
         try {
-            const thought = Thought.findOneAndDelete({ _id: req.params.thoughtId })
+            const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId })
             if (!thought) return res.status(404).json({ message: 'No thought found' })
 
         } catch (err) {
             console.error({ message: err })
-            res.status(500).json(err)
+            return res.status(500).json(err)
         }
     },
 
     async updateThought(req, res){
         try {
-            const thought = Thought.findOneAndUpdate(
+            const thought = await Thought.findOneAndUpdate(
                 { _id: req.params.thoughtId },
                 { $set: req.body },
                 { runValidators: true, new: true }
             )
         
             if (!thought) return res.status(404).json({ message: 'No thought found' })
-            res.json(thought)
+            return res.json(thought)
 
         } catch (err) {
             console.error({ message: err })
-            res.status(500).json(err)
+            return res.status(500).json(err)
         }
     },
 
     async createReaction(req, res){
         try {
-            const thought = Thought.findOneAndUpdate(
+            const thought = await Thought.findOneAndUpdate(
                 { _id: req.params.thoughtId },
                 { $addToSet: { reactions: req.body } },
                 { runValidators: true, new: true }
@@ -75,20 +75,20 @@ module.exports = {
             if(!thought) return res.status(404).json({ message: 'No thought found' })
         } catch (err) {
             console.error({ message: err })
-            res.status(500).json(err)
+            return res.status(500).json(err)
         }
     },
 
     async deleteReaction(req, res){
         try {
-            const thought = Thought.findOneAndUpdate(
+            const thought = await Thought.findOneAndUpdate(
                 { _id: req.params.thoughtId },
                 { $pull: { reactions: { reactionId: req.parms.reactionId }}},
                 { runValidators: true, new: true}
             )
         } catch (err) {
             console.error({ message: err })
-            res.status(500).json(err)
+            return res.status(500).json(err)
         }
     }
 }
